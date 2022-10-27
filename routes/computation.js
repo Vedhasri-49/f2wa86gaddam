5b802b9b5a25e5f974d440de1f19f25f0d4ea9a1
Vedhasri-49 <112114968+Vedhasri-49@users.noreply.github.com>
2022-10-26 23:42:20 -0500

    updated bonus

--- routes/computation.js
+++ routes/computation.js
@@ -2,12 +2,12 @@
 var express = require('express');
 var router = express.Router();
 var x = Math.random();
-var y = Math.random();
+
 var val1 = "Math.atan(x) applied to " + x + " is " + Math.atan(x)
 var val2 = "Math.exp(x) applied to " + x + " is " + Math.exp(x)
 var val3 = "Math.expm1(x) applied to " + x + " is " + Math.expm1(x);
 /* GET home page. */
 router.get('/', function (req, res, next) {
-    res.render('computation', { title: 'VEDHA SRI GADDAM   ', value1: val1, value2: val2, value3: val3 });
+    res.render('computation', { title: 'VEDHA SRI GADDAM', value1: val1, value2: val2, value3: val3 });
 });
 module.exports = router;
